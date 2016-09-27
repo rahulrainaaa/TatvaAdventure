c263c735e95903a35456669d7c0b4636a35e051d
rahulrainahomeee <rahulrainahome@gmail.com>
2016-09-27 17:48:35 +0530

    Initial commit

---
+++ app/src/main/java/com/tatva/tatvaadventure/Employee.java
@@ -0,0 +1,9 @@
+package com.tatva.tatvaadventure;
+
+public class Employee {
+
+	public Integer id;
+	public String firstName;
+	public String lastName;
+
+}
--- app/src/main/java/com/tatva/tatvaadventure/SplashActivity.java
+++ app/src/main/java/com/tatva/tatvaadventure/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.tatva.tatvaadventure;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONObject;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        try {
+            JSONObject json = new JSONObject();
+            json.put("id", 2);
+            json.put("firstName", "hello");
+            json.put("lastName", "world");
+
+            byte[] jsonData = json.toString().getBytes();
+
+            ObjectMapper mapper = new ObjectMapper();
+            Employee employee = mapper.readValue(jsonData,
+                    Employee.class);
+            System.out.println(employee.id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+        }
     }
 }

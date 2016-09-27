8d25baed6eb15a6b55af8a1568ebb92dbe9744b3
rahulrainahomeee <rahulrainahome@gmail.com>
2016-09-27 17:59:58 +0530

    Initial commit

--- app/src/main/java/com/tatva/tatvaadventure/ASyncActivateDevice.java
+++ app/src/main/java/com/tatva/tatvaadventure/ASyncActivateDevice.java
@@ -2,9 +2,7 @@ package com.tatva.tatvaadventure;
 
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.widget.Toast;
 
 import org.json.JSONObject;
 
@@ -23,26 +21,10 @@ public class ASyncActivateDevice extends AsyncTask<String, String, String> {
     private Context context;
     private JSONObject jsonRequest;
 
-    public ASyncActivateDevice(Context context)
-    {
-        this.context = context;
-    }
 
     @Override
     protected void onPreExecute() {
 
-        //Toast.makeText(context, "Registering", Toast.LENGTH_LONG).show();
-        jsonRequest = new JSONObject();
-        try
-        {
-            SharedPreferences s = context.getSharedPreferences("info_cache", 0);
-            String pref = s.getString("GCM_ID", "NO").trim();
-            if(!pref.equals("NO"))
-            {
-                jsonRequest.put("DEVICEID", pref.toString());
-            }
-        }
-        catch (Exception e){}
         super.onPreExecute();
     }
 
@@ -52,7 +34,7 @@ public class ASyncActivateDevice extends AsyncTask<String, String, String> {
         try {
             //Create HttpURLConnection and set request properties
             URL url = new URL(Constants.REGISTER_DEVICE);
-            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setDoInput(true);
             urlConnection.setDoOutput(true);
             urlConnection.setRequestProperty("Content-Type", "application/json");
@@ -70,27 +52,21 @@ public class ASyncActivateDevice extends AsyncTask<String, String, String> {
             urlConnection.connect();
             int statusCode = urlConnection.getResponseCode();
             String str = urlConnection.getResponseMessage();
-            if (statusCode ==  200)
-            {
+            if (statusCode == 200) {
                 InputStream it = new BufferedInputStream(urlConnection.getInputStream());
                 InputStreamReader read = new InputStreamReader(it);
                 BufferedReader buff = new BufferedReader(read);
                 StringBuilder dta = new StringBuilder();
-                String chunks ;
-                while((chunks = buff.readLine()) != null)
-                {
+                String chunks;
+                while ((chunks = buff.readLine()) != null) {
                     dta.append(chunks);
                 }
                 return dta.toString();
-            }
-            else
-            {
+            } else {
                 return "ERROR";
             }
 
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             return "EXCEPTION";
         }
 
@@ -99,27 +75,6 @@ public class ASyncActivateDevice extends AsyncTask<String, String, String> {
     @Override
     protected void onPostExecute(String s) {
 
-        try {
-            JSONObject response = new JSONObject(s.toString());
-            if (response.getString("statusCode").contains("200"))
-            {
-                SharedPreferences.Editor se = context.getSharedPreferences("info_cache", 0).edit();
-                se.putString("REGISTER","YES");
-                se.commit();
-                Toast.makeText(context, "Device Registered Successfully.", Toast.LENGTH_SHORT).show();
-            }
-            else if (response.getString("statusCode").contains("300"))
-            {
-                Toast.makeText(context, "" + response.getString("data").toString(), Toast.LENGTH_SHORT).show();
-            }
-            else if (response.getString("statusCode").contains("400"))
-            {
-                Toast.makeText(context, "" + response.getString("data").toString(), Toast.LENGTH_SHORT).show();
-            }
-
-        } catch (Exception e) {
-            Toast.makeText(context, "Please check internet connection.", Toast.LENGTH_LONG).show();
-        }
 
         super.onPostExecute("");
     }
--- app/src/main/java/com/tatva/tatvaadventure/Constants.java
+++ app/src/main/java/com/tatva/tatvaadventure/Constants.java
@@ -7,22 +7,7 @@ public class Constants
     public static String PROJECT_NUMBER = "193565881799";
     public static String API_KEY = "AIzaSyBR7CXvC8-BK5MqrFfxT_f0N8Fb00qCxHk";
 
-    //General constant values
-    public static String PHONE = "123";
-    public static String WEB = "http://www.google.com";
-    public static String MAIL = "promobilepune@gmail.com";
-
-    //Address lines
-    public static String ADDRESS_LINE_1 = "Address line 1 ";
-    public static String ADDRESS_LINE_2 = "Address line 2";
-    public static String ADDRESS_LINE_3 = "Address line 3";
-
-    //Address latitude, longitude
-    public static String ADDRESS_LAT_LONG = "18.505498, 73.802596";
-
-    //WebServices urls
-    //public static String REGISTER_DEVICE = "http://rahul.mulkindia.in/ProMobileWebService/activateApp/activate";
-
-    public static String REGISTER_DEVICE = "http://rahul.mulkindia.in/ProMobileWebService/activateApp/activate";
+    //WebServices
+    public static String REGISTER_DEVICE = "http://localhost:8080";
 
 }

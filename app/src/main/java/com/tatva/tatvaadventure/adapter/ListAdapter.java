09b58ac900f7ff3ee4a739ea3d7ac7ace18c9646
rahulrainahomeee <rahulrainahome@gmail.com>
2016-09-30 11:52:20 +0530

    Initial Commit

--- app/src/main/java/com/tatva/tatvaadventure/adapter/ListAdapter.java
+++ app/src/main/java/com/tatva/tatvaadventure/adapter/ListAdapter.java
@@ -1,15 +1,63 @@
 package com.tatva.tatvaadventure.adapter;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.tatva.tatvaadventure.R;
 import com.tatva.tatvaadventure.model.EventDetail;
 
 import java.util.ArrayList;
 
 public class ListAdapter extends ArrayAdapter<EventDetail> {
 
+    ArrayList<EventDetail> list = null;
+    Activity activity = null;
+    LayoutInflater inflater = null;
+
+    static class ViewHolder
+    {
+        public TextView txtDate;
+        public TextView txtTitle;
+        public TextView txtPlace;
+     }
+
     public ListAdapter(Activity activity, ArrayList<EventDetail> list) {
-        super(activity, android.R.layout.simple_list_item_1, list);
+        super(activity, R.layout.item_list_events, list);
+        this.list = list;
+        this.activity = activity;
+        this.inflater = activity.getLayoutInflater();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = convertView;
+        ViewHolder viewHolder;
+        if (view == null)
+        {
+            view = inflater.inflate(R.layout.item_list_events, null);
+            viewHolder = new ViewHolder();
+            viewHolder.txtDate = (TextView)view.findViewById(R.id.txtDate);
+            viewHolder.txtTitle = (TextView)view.findViewById(R.id.txtTitle);
+            viewHolder.txtPlace = (TextView)view.findViewById(R.id.txtPlace);
+            view.setTag(viewHolder);
+        }
+        else
+        {
+            viewHolder = (ViewHolder)view.getTag();
+        }
+        TextView txtDate = viewHolder.txtDate;
+        TextView txtTitle = viewHolder.txtTitle;
+        TextView txtPlace = viewHolder.txtPlace;
+
+        txtDate.setText("date " + position);
+        txtTitle.setText("Title " + position);
+        txtPlace.setText("Place " + position);
+
+        return view;
     }
 }

c15b2db8372e24e4ab2b689b814756db52b09363
rahulrainahomeee <rahulrainahome@gmail.com>
2016-10-01 14:08:00 +0530

    Initial Commit

--- app/src/main/java/com/tatva/tatvaadventure/activity/EventsActivity.java
+++ app/src/main/java/com/tatva/tatvaadventure/activity/EventsActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.tatva.tatvaadventure.R;
@@ -26,29 +30,39 @@ public class EventsActivity extends AppCompatActivity implements AdapterView.OnI
     private ListView listView = null;
     ArrayList<EventDetail> list = new ArrayList<EventDetail>();
     ListAdapter adapter = null;
+    ImageView img = null;
+    ProgressBar progress = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
         listView = (ListView) findViewById(R.id.listView);
+        img = (ImageView)findViewById(R.id.img);
+        progress = (ProgressBar)findViewById(R.id.process);
         listView.setOnItemClickListener(this);
         listView.setOnItemLongClickListener(this);
         adapter = new ListAdapter(this, list);
         listView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
-        try {
-            JSONObject json = new JSONObject();
-            json.put("auth", Constants.AUTH_GET);
-            ASyncHttpHandler httpHandler = new ASyncHttpHandler(this, Constants.URL_GET, json, 1);
-            httpHandler.execute("");
-        } catch (Exception e) {
-            Toast.makeText(EventsActivity.this, "Application crashing internally", Toast.LENGTH_SHORT).show();
-        }
-
+        refreshList();
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        refreshList();
+        return true;
+    }
+
+                @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
         Constants.eventDetail = list.get(position);
@@ -64,7 +78,8 @@ public class EventsActivity extends AppCompatActivity implements AdapterView.OnI
 
     @Override
     public void onHttpSuccess(int statusCode, String statusMessage, String data, int tag) {
-
+        progress.setVisibility(View.GONE);
+        img.setVisibility(View.GONE);
         try {
             JSONObject json = new JSONObject(data);
             int code = json.getInt("statusCode");
@@ -102,11 +117,33 @@ public class EventsActivity extends AppCompatActivity implements AdapterView.OnI
     @Override
     public void onHttpFail(int statusCode, String statusMessage, int tag) {
         Toast.makeText(EventsActivity.this, statusCode + ": " + statusMessage, Toast.LENGTH_SHORT).show();
+        progress.setVisibility(View.GONE);
+        img.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void onHttpError(int tag) {
         Toast.makeText(EventsActivity.this, "Server Connection Fail", Toast.LENGTH_SHORT).show();
+        progress.setVisibility(View.GONE);
+        img.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * @method refreshList
+     * @desc Method to Call Web-service for list
+     */
+    private void refreshList()
+    {
+        progress.setVisibility(View.VISIBLE);
+        img.setVisibility(View.GONE);
+        try {
+            JSONObject json = new JSONObject();
+            json.put("auth", Constants.AUTH_GET);
+            ASyncHttpHandler httpHandler = new ASyncHttpHandler(this, Constants.URL_GET, json, 1);
+            httpHandler.execute("");
+        } catch (Exception e) {
+            Toast.makeText(EventsActivity.this, "Application crashing internally", Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
